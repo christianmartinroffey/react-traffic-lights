1543a5a0951a9af437025e6aa426445eb9edf737
christianmartinroffey <c.martinroffey@gmail.com>
2022-05-29 18:42:12 +0000

    added the ternary operators for the glow state

--- src/js/component/TrafficLights.js
+++ src/js/component/TrafficLights.js
@@ -1,26 +1,41 @@
 import React, { useState } from "react";
 
 const TrafficLights = (props) => {
-	const [color, setColor] = useState("red");
+	const [Color, setColor] = useState("red");
 
 	return (
 		<div className="container d-flex justify-content-center pt-5 ">
 			<div className="bg-dark rounded-3 p-3">
-				<div>thestick</div>
-				<div className="light glow rounded-circle bg-danger p-5 m-1">
+				<div></div>
+				<div
+					onClick={() => setColor("red")}
+					className={
+						"light rounded-circle bg-danger p-5 m-1 " +
+						(Color === "red" ? "glow" : "")
+					}>
 					red
 				</div>
-				<div className="light rounded-circle bg-warning p-5 m-1">
+				<div
+					onClick={() => setColor("yellow")}
+					className={
+						"light rounded-circle bg-warning p-5 m-1 " +
+						(Color === "yellow" ? "glow" : "")
+					}>
 					yellow
 				</div>
-				<div className="light rounded-circle bg-success p-5 m-1">
+				<div
+					onClick={() => setColor("green")}
+					className={
+						"light rounded-circle bg-success p-5 m-1 " +
+						(Color === "green" ? "glow" : "")
+					}>
 					green
 				</div>
-				<button
+				{/* <button
 					className="bg-light d-flex justify-content-center d-flex align-items-center mt-5"
 					onClick={() => setColor()}>
 					Click me
-				</button>
+				</button> */}
 			</div>
 		</div>
 	);
