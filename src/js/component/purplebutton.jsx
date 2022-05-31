8156f5291243362c19c94ca139579de38b8af040
christianmartinroffey <c.martinroffey@gmail.com>
2022-05-31 10:04:34 +0000

    add click

---
+++ src/js/component/purplebutton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Purple = (props) =>
+{ return( <button
+	className="bg-light d-flex justify-content-center d-flex align-items-center mt-5"
+	    onClick={() => setColor()}>
+		    Click me
+    </button>
+    
+);}
+
+export default Purple;
